cbc6316160730309c012462ba12fc355b328f56c
jussihuotari <jussihuotari@users.noreply.github.com>
2015-09-16 16:25:24 +0300

    Provide undo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     to_keep = params[:to_keep]
     logger.info("Keeping #{@book.title}: #{to_keep}")
-    redirect_to @book.next
+    previous = {id: @book.id, title: @book.title.truncate(50), choice: to_keep}
+    redirect_to @book.next, flash: {previous: previous} # http://guides.rubyonrails.org/action_controller_overview.html#session
   end
 end
