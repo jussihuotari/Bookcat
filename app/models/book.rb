47f3f06b56d3950be05d3993be36b14faed3d059
jussihuotari <jussihuotari@users.noreply.github.com>
2015-11-18 23:11:04 +0200

    Make the layout more dense, add some styling. Works muchos better on mobile now.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ActiveRecord::Base
   validates :book_uuid, uniqueness: true
-  paginates_per 15
+  paginates_per 16
   def next
     Book.where(["id > ?", id]).order(id: :asc).first
   end
